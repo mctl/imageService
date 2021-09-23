32907d095c5f6c5f6ed57e0924bfbe88a15c65cb
mctl <ssg0629@163.com>
2021-09-23 17:23:58 +0800

    feat

--- src/main/java/com/zetyun/controller/uploadController.java
+++ src/main/java/com/zetyun/controller/uploadController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author: Create by gaosh
  * @date: 2/9/21
@@ -27,10 +31,17 @@ public class uploadController {
 //    @CrossOrigin(origins = "*", maxAge = 3600)
     public JSONObject uploadImage(@RequestParam("file") MultipartFile file) {
         String fileName = fileService.storeFile(file);
+        List<String> imgsType = new ArrayList<>(Arrays.asList("image/jpg", "image/jpeg", "image/png"));
+
 
         JSONObject result = new JSONObject();
+        JSONObject data = new JSONObject();
+        data.put("name", fileName);
+        data.put("path", "files/upload/" + fileName);
+        data.put("isImage", imgsType.contains(file.getContentType()));
+
         result.put("code", "1");
-        result.put("data",fileName);
+        result.put("data",data);
         return result;
     }
 }
